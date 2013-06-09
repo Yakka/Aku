225aedfb6978178594c274049f5c0dbd89c1c19c
M K <michele@Ordinator.local>
2013-06-09 21:56:51 +0200

    before merge for release again

--- Assets/Scripts/Ladybird.cs
+++ Assets/Scripts/Ladybird.cs
@@ -326,6 +326,7 @@ public class Ladybird : MonoBehaviour
 				animation.PlayQueued("land", QueueMode.CompleteOthers);
 				
 				// MICHELE son de "coccinelle attend que tu fasses quelque chose"
+				CommonSounds.Instance.LadybirdShake();
 			}	
 			
 			if(index > targets.Length - 1)
